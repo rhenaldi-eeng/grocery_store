b8682e802f2afacc7900b4d85e09a5c75de9cc3a
rhenaldi wijaya <wijayaashif@gmail.com>
2023-01-20 22:53:46 -0700

    modify cursor source

--- backend/products_dao.py
+++ backend/products_dao.py
@@ -3,4 +3,15 @@ import mysql.connector
 cnx = mysql.connector.connect(user='root', password='Tigrex171328!',
                               host='127.0.0.1',
                               database='grocery_store')
+
+cursor = cnx.cursor()
+
+query = ("select products.product_id, products.name, products.uom_id, products.price_per_unit, uom.uom_name"
+         "from products inner join uom on products.uom_id=uom.uom_id;")
+
+cursor.execute(query)
+
+for (product_id, name, uom_id, price_per_unit, uom_name) in cursor:
+    print(product_id, name, uom_id, price_per_unit, uom_name)
+
 cnx.close()
