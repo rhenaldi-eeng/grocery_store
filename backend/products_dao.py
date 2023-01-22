8f66ad76de420a4c460581548c238a26cb3a66da
rhenaldi wijaya <wijayaashif@gmail.com>
2023-01-22 13:16:45 -0700

    add insert_product function

--- backend/products_dao.py
+++ backend/products_dao.py
@@ -25,6 +25,25 @@ def retreive_products(connection):
         )
     return response 
 
+def insert_product(connection, product):
+    cursor = connection.cursor()
+    add_product = (
+        "INSERT INTO products " 
+        "(name,uom_id, price_per_unit)"
+        "VALUES (%s, %s, %s)"
+    )
+
+    data = (product['product_name'], product['uom_id'], product['price_per_unit'])
+
+    cursor.execute(add_product, data)
+    connection.commit()
+
+    return None
+
 if __name__ == '__main__':
     connection = retreive_sql_connection()
-    print(retreive_products(connection))
+    print(insert_product(connection, {
+        'product_name': 'mango',
+        'uom_id' : '2',
+        'price_per_unit' : '20'
+    }))
